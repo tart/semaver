e717af804b0168e50f861858b3099545dd3fec81
dashersw <armagan@amcalar.com>
2014-05-09 04:14:21 +0300

    Fix code style in client example and server lib.

--- example/client.js
+++ example/client.js
@@ -1,14 +1,14 @@
 var client = require('../lib/client');
 var id = process.argv[2];
 
-client.connect(function(){
-	client.acquire(id, function() {
-	    console.log('obtained lock', id);
+client.connect(function() {
+    client.acquire(id, function() {
+        console.log('obtained lock', id);
 
-	    setTimeout(function() {
-	        console.log('unlocking', id);
-	        client.release(id);
-	    }, 10000);
-	});
+        setTimeout(function() {
+            console.log('unlocking', id);
+            client.release(id);
+        }, 10000);
+    });
 });
 
